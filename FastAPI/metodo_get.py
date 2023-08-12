44e37f414bed7e58444438972f8575d5b336d71b
kimera <angeljimenez7606@gmail.com>
2023-08-12 09:43:56 -0400

    se continua con le aprendizaje

--- FastAPI/metodo_get.py
+++ FastAPI/metodo_get.py
@@ -1,12 +1,26 @@
 #empezemos a usar los metodos http 
 from fastapi import FastAPI
+#podemso tambien importar html pero debemos hacer un from:
 from fastapi.responses import HTMLResponse #para importar html 
 pag = FastAPI()
 pag.title = "metodos HTTP con FastAPI"
 pag.version = "1.2"
+
+movies = [
+    {
+        "id" : 1,
+        "titulo" : "El Señor De Los Anillos",
+        "yer ": 2000,
+        "categoria" : "fantasia"
+     }
+]
 
 @pag.get("/", tags=["catalogo"])
 
 def msj ():
     return HTMLResponse("<h1> Hola </h> ") #como podemos ver es un operador 
 
+@pag.get("/Movies", tags= ["movies"] )
+
+def get_movies():
+    return movies
