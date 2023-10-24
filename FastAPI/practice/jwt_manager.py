281244fc06e83dd4536ff903e42eb64fedcd4bcd
kimera <angeljimenez7606@gmail.com>
2023-10-24 14:17:21 -0400

    se elimino carperta de fastapi

--- FastAPI/practice/jwt_manager.py
+++
@@ -1,8 +0,0 @@
-from jwt import encode, decode
-def create_tokken(data:dict) -> str:
-    token :str = encode(payload= data, key= "my_key", algorithm="HS256")
-    return token
-
-def validate_token (token:str) -> dict:
-    data : dict = decode(token, key= "my_key", algorithms=["HS256"])
-    return data
